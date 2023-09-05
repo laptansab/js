f6c2e5a226766635f12bf56ab3eb0dfe0252a3b8
laptansab <66240186+laptansab@users.noreply.github.com>
2023-09-05 03:40:29 +0000

    added assignment for prefix postfix

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -31,17 +31,16 @@ console.log(valueInNumber);
 //"" => false
 //"sushil" => true
 
-let someNumber = 333
+let someNumber = 333;
 
-let strringNumber = String(someNumber)
+let strringNumber = String(someNumber);
 // console.log(strringNumber);
 // console.log(typeof strringNumber);
 
 // ************************** Operation****************
 
-
-let value = 3
-let negValue = -value
+let value = 3;
+let negValue = -value;
 // console.log(negValue);
 // console.log(2+2);
 // console.log(2*2);
@@ -50,10 +49,10 @@ let negValue = -value
 // console.log(2/5);
 // console.log(2%3);
 
-let str1 = "hello "
-let str2 = "sushil"
+let str1 = "hello ";
+let str2 = "sushil";
 
-let str3 = str1 + str2
+let str3 = str1 + str2;
 // console.log(str3);
 
 // console.log("1" + 2); ==> 12
@@ -63,3 +62,17 @@ let str3 = str1 + str2
 // console.log(1 + 2 + "2");// ==> 32
 // console.log(3+((4*5)%3)); // for 3+4*5%3 ==> 5
 // console.log(3+(4*(5%3))); // ==> 11
+
+// console.log(+true); // ==> 1
+// console.log(+""); // ==> 0
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+// link to stydy 
+// http://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-vconversion
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # js
 code repo for javascript learning from chaiaurcode youtube  channel
+
+# 01 Basic
+
+## 03 Conversion Operations
+
+### Assignment
+
+[Link to study](https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-vconversion)
