852898b7bf24c36432181e6adb43931d54ba6d92
laptansab <66240186+laptansab@users.noreply.github.com>
2023-09-05 02:08:27 +0000

    conversion of dataType

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,38 @@
+/*
+let score = "33abc"
+let score = null
+let score = undefined
+let score = true
+
+const {scored} = req.bosy
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+//"33" => 33
+//"33sbc" => NaN(not a number)
+// tue => 2; false =>0
+//null => 0
+// undefined = NaN
+
+/*
+ let isLoggedIn = "sushil"
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+ console.log(booleanIsLoggedIn);
+*/
+//1 => true
+//0 => false
+//"" => false
+//"sushil" => true
+
+let someNumber = 333
+
+let strringNumber = String(someNumber)
+console.log(strringNumber);
+console.log(typeof strringNumber);
