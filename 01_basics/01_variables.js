9150f6c266ebd6d48a575a723e9ceb172eea39d8
laptansab <66240186+laptansab@users.noreply.github.com>
2023-09-05 01:14:44 +0000

    updated 02_dataTypes.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,26 @@
 const accountId = 144553
-let email = "sushil@google.com"
+let accountEmail = "sushil@google.com"
 var accountPassword = "12345@12345!"
 accountCity = "Jaipur"
+let accountState;
 
-accountId = 2
+// accountId = 2 // not allowed
+
+/*
+{
+    scope
+    local scope
+    global scope
+}
+
+prefer not to use var
+because of issues in block scope and functional scope
+
+*/
+
+accountEmail = "Suraj@g.com"
+accountPassword = "151515"
+accountCity = "ktm"
 
 console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version or new js
+
+// alert(3+3) // we are using node js not browser
+
+// console.log(3+3); console.log("sushil") //this is not good practic for readability
+
+// console.log(3 + 3);
+// console.log("sushil");
+
+let name = "sushil"
+let age = 22
+let isLoggedIn = false
+let state;
+
+//num => 2 to power 53
+//bigint
+//string => '' or "" 
+//boolean => true/false
+//null=> standalone value
+//undefined =>
+//symbol => unique
+
+//object => {[],[]}
+
+console.table([typeof name, typeof null,typeof age, typeof undefined,typeof isLoggedIn]);
