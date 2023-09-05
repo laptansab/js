3a5a4b36c124fb8f50884ca4823dfbd36193cf32
laptansab <66240186+laptansab@users.noreply.github.com>
2023-09-05 07:10:04 +0000

    string trim() and replace()

---
+++ 01_basics/05_string.js
@@ -0,0 +1,32 @@
+const  name = "sushil"
+const repoCount = 5;
+
+// console.log(name + message + " value");
+
+// console.log(`Hello, My name is ${name.toUpperCase()} and my repo count is ${repoCount} `);
+
+const gameName = new String("sushil")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toLocaleLowerCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("v")); // ==> -1
+
+// console.log(gameName.indexOf("V")); // ==> 4
+
+// let newString = gameName.substring(0,2)
+// console.log(newString);
+
+// let anotherString = gameName.slice(-5,4)
+// console.log(anotherString);
+
+// let uName = "       sushil chaulagain                "
+// console.log(uName);
+// console.log(uName.trim());
+
+// const url = "https://sushil.com/sushil%20chaulagain"
+// console.log(url);
+// console.log(url.replace('%20','-'));
