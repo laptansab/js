4921be6d251e972e106c2400c98ea5a57fd11359
laptansab <66240186+laptansab@users.noreply.github.com>
2023-09-06 07:06:02 +0000

    keys values entries hasOwnProperty

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -28,5 +28,39 @@ let obj2 = {
 
 // const obj3 = {obj1,obj2}
 // const obj3 = Object.assign(obj1,obj2)
-const obj3 = Object.assign({},  obj1,obj2) // assign(target,source)
-console.log(obj3);
+// const obj3 = Object.assign({},  obj1,obj2) // assign(target,source)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users =[
+  {
+  id:1,
+  email:"h@gmail.com",
+},
+  {
+  id:1,
+  email:"h@gmail.com",
+},
+{
+  id:1,
+  email:"h@gmail.com",
+},{
+  id:1,
+  email:"h@gmail.com",
+},{
+  id:1,
+  email:"h@gmail.com",
+},{
+  id:1,
+  email:"h@gmail.com",
+},
+] 
+
+// console.log(users[1].email);
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser), Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
